01cddb71a5ec1f09723395f89878ff752a753082
Tyrel M. McQueen <tmcqueen-materials@github.com>
2024-06-24 19:59:42 -0400

    Refactor handlers to enable pluggable new analysis workflows

--- autolaunch/handlers.py
+++ autolaunch/handlers.py
@@ -1,27 +1,21 @@
 from tornado import web
 from base64 import urlsafe_b64decode
 from shutil import copyfile
-
+from pathlib import Path
+from signal import SIGUSR1
 import subprocess
-
 import json
 import os
-from signal import SIGUSR1
+
+from io import BytesIO
+
+from .analysis import analysis_handlers
 
 from ._compat import get_base_handler
-
-from pathlib import Path
-
 JupyterHandler = get_base_handler()
 
 class AutoLaunchHandler(JupyterHandler):
-    analysis_notebooks_src = '/autolaunch/notebooks'
-    analysis_notebooks = {
-        "Default": "",
-        "PPMS-CW": "PPMS-CW.ipynb",
-        "MPMS-CW": "MPMS-CW.ipynb",
-        "XRD-Plot": "",
-    }
+    analysis_notebooks_src = '/autolaunch/notebooks' # TODO: should be learned by inference, but for now, hard-coded
     """
     The /autolaunch endpoint.
     """
@@ -30,17 +24,23 @@ class AutoLaunchHandler(JupyterHandler):
         await self._handle_autolaunch()
 
     # TODO: Asymmetrically encrypt auth_token from javascript front-end to prevent token stealing
-    # from its presence in the URL.
+    # from its presence in the URL (since we must use GET instead of POST in JupyterHub user-redirect).
     async def _handle_autolaunch(self):
         token = self.get_argument('auth_token', '')
-        files = self.get_argument('files', '')
-        files = json.loads(urlsafe_b64decode(files + '=' * ((4 - len(files)) % 4)))
+        files_urlfs = self.get_argument('files', '')
+        files_urlfs = json.loads(urlsafe_b64decode(files_urlfs + '=' * ((4 - len(files_urlfs)) % 4)))
 
         basedir = os.path.expanduser(self.settings['server_root_dir'])
         mountdir = os.path.join(basedir,'remote')
         analysis_subpath = 'analysis'
         analysisdir = os.path.join(basedir,analysis_subpath)
         configdir = os.path.join(basedir,'.remote-config')
+
+        # Get files as will appear to us, not as needed by urlfs
+        files = []
+        for f in files_urlfs:
+            if f.split('\t')[0] == 'F':
+                files.append(os.path.join(mountdir,f.split('\t')[1][1:]))
 
         # Ensure that requisite directories exist
         if not os.path.isdir(analysisdir):
@@ -60,8 +60,9 @@ class AutoLaunchHandler(JupyterHandler):
                 f.write("\t")
                 f.write("X-Auth-Access-Token: " + token)
             f.write("\n")
+
         with open(idxfile,"w") as f:
-            for fil in files:
+            for fil in files_urlfs:
                 f.write(fil + "\n")
 
         # mount urlfs if not already mounted
@@ -76,29 +77,26 @@ class AutoLaunchHandler(JupyterHandler):
                 os.kill(pid,SIGUSR1)
 
         analysis_hint = self.get_argument('analysis_hint','')
-        if not (analysis_hint in self.analysis_notebooks):
-            analysis_hint = 'Default'
+        if not (analysis_hint in analysis_handlers):
             # Try to figure out which type of analysis notebook we need to copy over
-            if len(list(Path(mountdir).glob("*.raw"))) > 0:
-                analysis_hint = "XRD-Plot"
-            elif len(list(Path(mountdir).glob("*.dat"))) > 0:
-                fil = list(Path(mountdir).glob("*.dat"))[0]
+            for fil in files:
+                # once we have figured out one, we are done. If working with multiple file types,
+                # autolaunch is called multiple times, once for each file type.
+                if len(analysis_hint) > 0:
+                    break
+                # read only first part of file for ID purposes. 64k should be enough for anyone
+                pdat = ''
                 with open(fil, "rb") as f:
-                    for nv in f:
-                        if b'MPMS3' in nv:
-                            analysis_hint = 'MPMS-CW'
-                            break
-                        elif b'PPMS ACMS' in nv:
-                            analysis_hint = 'PPMS-CW'
-                            break
+                    pdat = f.read(65536)
+                # iterate over analysis handlers to find a suitable one
+                for ah in analysis_handlers:
+                    if analysis_handlers[ah].checkIsDataType(pdat, fil):
+                        analysis_hint = ah
+                        break
 
-        nb = self.analysis_notebooks[analysis_hint]
-        if len(nb) > 0:
-            if not os.path.isfile(os.path.join(analysisdir,nb)):
-                # copy analysis book if not already copied
-                copyfile(os.path.join(self.analysis_notebooks_src,nb), os.path.join(analysisdir,nb))
-                os.chmod(os.path.join(analysisdir,nb), 0o644)
-            return_url = self.base_url + 'lab/tree/' + analysis_subpath + '/' + nb
+        if analysis_hint in analysis_handlers:
+            analysis_handlers[analysis_hint].copyTemplate(analysisdir, self.analysis_notebooks_src)
+            return_url = self.base_url + 'lab/tree/' + analysis_subpath + '/' + analysis_handlers[analysis_hint].getAnalysisFileName()
         else:
             return_url = self.base_url + 'lab/'
 
