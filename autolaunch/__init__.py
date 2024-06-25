aa11c10c61514f39769823a3ec1f2c8540b4f919
Tyrel M. McQueen <tmcqueen-materials@github.com>
2024-06-25 13:32:46 -0400

    Add documentation of TODOs and caveats.

--- autolaunch/__init__.py
+++ autolaunch/__init__.py
@@ -1,3 +1,25 @@
+"""
+    A full autolaunch implementation that allows one-click Jupyter servers to automatically pull in data and analysis notebook(s)
+    to analyze data. Handles fusion of multiple disparate data sources with separate authentications as required, by leveraging
+    URLFS as the file backend.
+
+    TODO:
+    1. Due to (re)writing of files on disk on the server, these endpoints are currently *not thread-safe*, and *will fail
+       catastrophically* if multiple endpoints are handling requests in parallel. In current usage, this is not a problem,
+       but this should be rectified at some point.
+    2. Right now, the number of files added in a single call is limited by the maximum length of a GET request (typically ~8 kB).
+       Adding the OTS ("one-time storage") callback can remove this limitation.
+    3. Right now, the only auth provider supported is polyauth (as used on data.paradim.org). An oauth2 implementation should
+       be added, to enable accessing most data from most places (since most places support oauth2).
+    4. Right now, secrets are stored local to the jupyter server, but otherwise unprotected. They should be stored in a
+       device-bound session as that becomes more widely supported.
+    5. There are a couple of configuration parameters (notebook source directory, names of key files) that should be
+       stored in extension settings and adjustable in the usual way for a jupyter extension.
+    6. Right now the glue logic for refreshing authentication tokens is handled within notebooks. This extension should
+       proactively watch for IO errors on URLFS and automatically kick off authentication refreshes when needed.
+
+"""
+
 from jupyter_server.utils import url_path_join
 from tornado.web import StaticFileHandler
 import os
